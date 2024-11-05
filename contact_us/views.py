c6b1b53eb4ad762fcc314f3231e104281d57a0a2
asirff399 <aintisar48@gmail.com>
2024-11-05 19:05:49 +0600

    first commit

---
+++ contact_us/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Contact
+from .serializers import ContactUsSerializer
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+class ContactViewset(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactUsSerializer
+
+    def perform_create(self, serializer):
+        contact = serializer.save()
+
+        email_subject = "Portfolio Contact Mail"
+        email_body = render_to_string('contact_mail.html',{'name':contact.name,'email':contact.email,'message':contact.message})
+        email = EmailMultiAlternatives(subject=email_subject,body='',to=['aintisar48@gmail.com'])
+        email.attach_alternative(email_body,"text/html")
+        email.send()
+
+        return Response({'message': 'Your message has been sent! Check your email for confirmation.'},status=status.HTTP_201_CREATED)
