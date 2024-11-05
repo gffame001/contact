3a324ee7787bed302e5b2c8ada4a72700bd597dc
asirff399 <aintisar48@gmail.com>
2024-11-05 21:15:02 +0600

    [Add] - postgresql

--- contact/settings.py
+++ contact/settings.py
@@ -90,12 +90,26 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.fkofiomaebjhodmnfvcl',
+        'PASSWORD': 'sLdwLMxvNsdDNnbN',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543'
     }
 }
+
+
+# sLdwLMxvNsdDNnbN
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
